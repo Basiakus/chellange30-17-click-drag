77d49e232a27230352b00cab46aedfa378dd626a
Adam Basiak <basiakus@gmail.com>
2018-08-28 21:19:08 +0200

    pushing

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const items = document.querySelector('.items');
+let mouse = false;
+let startX;
+let scrollLeft;
+
+items.addEventListener('mouseup', () => {
+	mouse = false;
+	items.classList.remove('active');
+});
+
+items.addEventListener('mousedown', (e) => {
+	mouse = true;
+	items.classList.add('active');
+	startX = e.pageX - items.offsetLeft;
+	scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+	mouse = false;
+});
+
+items.addEventListener('mousemove', (e) => {
+	e.preventDefault();
+	if(!mouse) return;
+	const x = e.pageX - items.offsetLeft;
+	const passing = x - startX;
+	items.scrollLeft = (scrollLeft - passing)*2;
+	console.log(`startX:${startX} scrollLeft:${scrollLeft} passing:${passing}`);
+});
